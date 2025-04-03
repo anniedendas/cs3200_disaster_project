50f3ecc18ccf3a9faaedb82b1a8e7694670c8366
Annie Dendas <94186312+anniedendas@users.noreply.github.com>
2025-04-03 14:38:30 -0400

    Create create_db.sql

---
+++ create_db.sql
@@ -0,0 +1,30 @@
+set global local_infile = on;
+
+-- create the database
+drop database if exists disaster;
+create database disaster;
+use disaster;
+
+-- create the gdp per capita table
+
+-- populate the gdp per capita table
+
+-- create the urban population table
+
+-- populate the urban population table
+
+-- create the literacy rates table
+
+-- populate the literacy rates table
+
+-- create the life expectancy table
+
+-- populate the life expectancy table
+
+-- create the international tourism table
+
+-- populate the international tourism table
+
+-- create the mean annual pollution table
+
+-- populate the mean annual pollution table
