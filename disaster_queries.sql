e7f299303564f251817724d71a3f58f2f1021dd6
Annie Dendas <94186312+anniedendas@users.noreply.github.com>
2025-04-13 16:16:12 -0400

    Add files via upload

---
+++ disaster_queries.sql
@@ -0,0 +1,105 @@
+use disasters;
+-- PLEASE NOTE THAT ALL QUERIES BELOW ARE USING NATURAL DISASTER EVENTS FROM 1960-2025
+
+-- query one: does a higher average GDP per capita (in constant 2015 USD) correlate with fewer 
+-- deaths per flood event among countries that have experienced at least three flood disasters,
+-- aggregated at the country level?
+select 
+    e.iso,
+    i.country_name,
+    round(avg(g.gdp_per_capita), 2) as avg_gdp_per_capita,
+    avg(e.total_deaths) as avg_deaths_per_flood,
+    count(*) as num_floods
+from emdat e
+join gdp g on e.iso = g.iso_code and e.start_year = g.year
+join iso i on e.iso = i.iso_code
+where 
+    e.total_deaths is not null 
+    and g.gdp_per_capita is not null
+    and e.disaster_type = 'Flood'
+group by 
+    e.iso, i.country_name
+having num_floods >= 3
+-- remove 'desc' to see countries w/ least amt of deaths first
+order by avg_deaths_per_flood desc;
+
+
+-- query two: is there a correlation between literacy rates of a country and the number of people 
+-- rendered homeless by tropical cyclones?
+select 
+    e.iso,
+    i.country_name,
+    e.start_year,
+    round(l.rate, 2) as literacy_rate,
+    count(e.disaster_id) as num_cyclones,
+    sum(e.no_homeless) as total_homeless,
+    round(sum(e.no_homeless) / count(e.disaster_id), 2) as avg_homeless_per_cyclone
+from emdat e
+join lit_rates l on e.iso = l.iso_code and e.start_year = l.year
+join iso i on e.iso = i.iso_code
+where 
+    e.no_homeless is not null
+    and l.rate is not null
+    and e.disaster_subtype = 'Tropical cyclone'
+group by 
+    e.iso, i.country_name, e.start_year, l.rate
+order by avg_homeless_per_cyclone desc, num_cyclones desc;
+
+
+-- query 3: how does the average number of disasters per year relate to a country’s average 
+-- life expectancy?
+select 
+    e.iso,
+    i.country_name,
+    round(avg(l.expectancy), 2) as avg_life_expectancy,
+    round(count(e.disaster_id) / count(distinct e.start_year), 2) as avg_disasters_per_year
+from emdat e
+join life_exp l on e.iso = l.iso_code and e.start_year = l.year
+join iso i on e.iso = i.iso_code
+where l.expectancy is not null
+group by e.iso, i.country_name
+order by avg_disasters_per_year desc, avg_life_expectancy desc;
+
+-- query 4: do countries with increasing disaster rates show slower growth in life expectancy over time
+-- compared to countries with fewer disasters? (Do recurring disasters have a long-term impact on population
+-- health/ are some countries resilient despite frequent extreme events?)
+select 
+    e.iso,
+    i.country_name,
+    e.start_year,
+    count(e.disaster_id) as num_disasters,
+    l.expectancy as life_expectancy
+from emdat e
+join life_exp l on e.iso = l.iso_code and e.start_year = l.year
+join iso i on e.iso = i.iso_code
+where l.expectancy is not null
+group by e.iso, i.country_name, e.start_year, l.expectancy
+order by e.iso, e.start_year;
+
+-- query 5: do countries with higher GDP per capita maintain stronger life expectancy
+-- trends over time despite experiencing frequent natural disasters, compared to lower-income countries?
+select 
+    e.iso,
+    i.country_name,
+    e.start_year,
+    round(avg(g.gdp_per_capita), 2) as gdp_per_capita,
+    case 
+        when avg(g.gdp_per_capita) > 12695 then 1
+        when avg(g.gdp_per_capita) between 1136 and 12695 then 2
+        when avg(g.gdp_per_capita) < 1136 then 3
+        else null
+    end as income_group,
+    round(avg(l.expectancy), 2) as life_expectancy,
+    count(e.disaster_id) as num_disasters
+from emdat e
+join gdp g on e.iso = g.iso_code and e.start_year = g.year
+join life_exp l on e.iso = l.iso_code and e.start_year = l.year
+join iso i on e.iso = i.iso_code
+where 
+    g.gdp_per_capita is not null
+    and l.expectancy is not null
+group by 
+    e.iso, i.country_name, e.start_year
+order by 
+    e.iso, e.start_year;
+
